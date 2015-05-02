784d2383e1b9192195baf0927e38f9b16bcae9cb
krpatel21 <krpatel21@outlook.com>
2015-05-02 07:49:13 -0500

    Test

---
+++ Account/src/Account.java
@@ -0,0 +1,71 @@
+//Kaushal Patel
+//CS 207
+import java.util.Date;
+
+public class Account 
+{	
+	//data members
+	private int id;
+	private double balance;
+	private double annualInterestRate;
+	private Date dateCreated = new Date();
+	//methods
+	public Account()
+	{
+		id = 0;
+		balance = 0;
+		annualInterestRate = 0;
+	}
+	public Account(int id, double balance)
+	{
+		this.id=id;
+		this.balance=balance;
+	}
+	//set methods
+	public void setId(int id)
+	{
+		this.id=id;	
+	}
+	public void setBalance(double balance)
+	{
+		this.balance=balance;	
+	}
+	public void setAnnualInterestRate(double annualInterestRate)
+	{
+		this.annualInterestRate = annualInterestRate/100;	
+	}
+	public Date getDateCreated()
+	{
+		return dateCreated;
+	}
+	//get methods
+	public int getId()
+	{
+		return id;
+	}
+	public double getBalance()
+	{
+		return balance;
+	}
+	public double getAnnualInterestRate()
+	{
+		return annualInterestRate;
+	}
+	public double getMonthlyInterest()
+	{
+		return balance*(annualInterestRate/12);
+	}
+	public double getMonthlyInterestRate()
+	{
+		return (annualInterestRate/12);
+	}
+	// withdraw and deposit
+	public void withdraw(double w)
+	{
+		balance = balance - w;
+	}
+	public void deposit(double d)
+	{
+		balance = balance + d;
+	}
+}
---
+++ Account/src/AccountApp.java
@@ -0,0 +1,38 @@
+//Kaushal Patel
+//CS 207
+public class AccountApp 
+{
+	public static void main(String[] args)
+	{		
+		// two argument constructor
+		Account account1 = new Account(1122, 20000);
+		//set method for interest rate
+		account1.setAnnualInterestRate(4.5);
+		account1.withdraw(2500);
+		account1.deposit(3000);		
+		System.out.println("output for account 1");
+		System.out.printf("The balance is $%.2f.%n",account1.getBalance());
+		System.out.printf("The interest is $%.2f.%n",account1.getMonthlyInterest());
+		System.out.println("The date is "+account1.getDateCreated());
+		// my own tests
+		Account account2 = new Account(0000, 99999);
+		account2.setAnnualInterestRate(11);
+		account2.withdraw(50000);
+		account2.deposit(90000);
+		System.out.println("\noutput for account 2");
+		System.out.printf("The balance is $%.2f.%n",account2.getBalance());
+		System.out.printf("The interest is $%.2f.%n",account2.getMonthlyInterest());
+		System.out.println("The date is "+account2.getDateCreated());
+	}	
+}
+//sample output
+/*output for account 1
+The balance is $20500.00.
+The interest is $76.88.
+The date is Thu Jan 29 13:20:28 CST 2015
+
+output for account 2
+The balance is $139999.00.
+The interest is $1283.32.
+The date is Thu Jan 29 13:20:28 CST 2015
+*/
